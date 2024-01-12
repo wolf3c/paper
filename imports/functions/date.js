943d73c274e0eefbd2b863153354bfc3ef474a2b
wolf3c <wolf3c@gmail.com>
2024-01-12 22:56:29 +0800

    fix Google RFC3339 格式要求

--- imports/functions/date.js
+++ imports/functions/date.js
@@ -2,12 +2,12 @@ import dayjs from 'dayjs';
 
 // 设置时间为当天0点
 export function startOfDay2rfc3339(dayOffset = 0) {
-    return dayjs().add(dayOffset, 'day').startOf('day').format()
+    return dayjs().add(dayOffset, 'day').startOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS[-08:00]');
 }
 
 // 设置时间为当天24点
 export function endOfDay2rfc3339(dayOffset = 0) {
-    return dayjs().add(dayOffset, 'day').endOf('day').format()
+    return dayjs().add(dayOffset, 'day').endOf('day').format('YYYY-MM-DDTHH:mm:ss.SSS[-08:00]');
 }
 
 // RFC 3339 timestamp 转化成 数字
